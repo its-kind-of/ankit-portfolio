b3107af92a90c79455e4441ac51433eb5df3b2c5
its-kind-of <nikhil.62ns@gmail.com>
2023-05-22 13:29:25 +0530

    first code push

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/inner')
+def inner():
+    return render_template('inner-page.html')
+
+@app.route('/details')
+def details():
+    return render_template('portfolio-details.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
+    
